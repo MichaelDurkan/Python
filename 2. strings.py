e8aaa204d2cee39d837e486fd7c16e772b4ca1b0
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-03-28 20:33:16 +0100

    Create 2. strings.py

---
+++ 2. strings.py
@@ -0,0 +1,40 @@
+print ("Michael\n Durkan")
+# creates a new line after the "n"
+
+print ("Michael Durkan")
+# prints the test out
+
+phrase = "Evan Durkan"
+print (phrase + " is cool")
+# creates a phrase variable and concatenates extra text into output
+
+phrase = "Evan Durkan"
+print (phrase.lower())
+# converts all text in the phrase variable to lowercase
+
+phrase = "Evan Durkan"
+print (phrase.upper())
+# converts all text in the phrase variable to uppercase
+
+phrase = "Evan Durkan"
+print (phrase.isupper())
+# Checks if all text in the phrase variable is in uppercase
+
+phrase = "Evan Durkan"
+print (phrase.upper().isupper())
+
+phrase = "Evan Durkan"
+print (len(phrase))
+# Counts the amount of characters in the phrase
+
+phrase = "Evan Durkan"
+print (phrase[0])
+# Gets the first letter in the string. Note that Python's index starts with 0, not 1!!
+
+phrase = "Evan Durkan"
+print (phrase.index("D"))
+# Shows what the index location for "D" is
+
+phrase = "Evan Durkan"
+print (phrase.replace("Evan", "Anna"))
+# Replaces the word "Evan" with the word "Anna"
