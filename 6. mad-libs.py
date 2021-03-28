04272af29c71ac73c39ba84e8b792a3a67afee77
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-03-28 20:44:33 +0100

    Create 6. mad-libs.py

---
+++ 6. mad-libs.py
@@ -0,0 +1,13 @@
+print("Roses are red")
+print("Violets are blue")
+print("I love you")
+# Above is basic text which can be outputted to the screen
+
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+print("Roses are " + color)
+print(plural_noun + "  are blue")
+print("I love " + celebrity)
+# Creates variables for our Mad Lib, ask the user for input and stores the input in the variables. The Mad Lib is now outputted on screen
