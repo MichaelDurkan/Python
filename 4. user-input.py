e174427c21dab2d75ce49ed0c2f38fb4418b5fcd
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-03-28 20:34:50 +0100

    Create 4. user-input.py

---
+++ 4. user-input.py
@@ -0,0 +1,7 @@
+name = input("Enter your Name: ")
+# Create a variable called name, and asks for input from the user
+age = input("Enter your Age: ")
+# Create a variable called age, and asks for input from the user
+
+print("Hello " + name + "! You are " + age)
+# Outputs the data entered by the user to the screen using the values the user entered which are stored in the variables
