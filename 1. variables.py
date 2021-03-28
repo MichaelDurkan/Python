8d4c1c4eb6ff5949056122f12d0aadb3c28a068f
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-03-28 20:30:48 +0100

    Create 1. variables.py

---
+++ 1. variables.py
@@ -0,0 +1,12 @@
+char_name = "Evan"
+char_age = "9"
+# Creates 2 variables, "char_name" and "char_age"
+print ("Hello my name is " + char_name +"")
+print ("I am " + char_age + " years old")
+# Creates 2 variables, "char_name" and "char_age"
+char_name = "Michael"
+char_age = "42"
+# Creates 2 new variables for the "char_name" and "char_age"
+print ("I like the name "+ char_name +"")
+print ("I do not like being " + char_age + " years old")
+# Creates 2 variables, "char_name" and "char_age"
