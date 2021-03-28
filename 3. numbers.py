af878d510d1cc577d5495981b4f29bcd9519280b
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-03-28 20:33:51 +0100

    Create 3. numbers.py

---
+++ 3. numbers.py
@@ -0,0 +1,38 @@
+import math
+# Imports all python math functions. its better to use this way "import math" instead of "from math import *", 
+
+print(2)
+# Prints a number on the screen
+
+print(3 + 4.5)
+# Prints out the result of adding the 2 numbers. You can also do subtract/multiple/divide
+
+print(3 * (4 + 5))
+# Uses parenthesis. The numbers inside the parenthesis are always calculated first. 
+
+print(10 % 3)
+# Modulist operation. This divides 10 by 3, then outputs the remainder
+
+mu_num =5
+print(mu_num)
+# Defines the number as a variable
+
+mu_num =5
+print(str(mu_num) + " is my favourite number")
+# Converts the number to a string. Useful when you need to print out a number alongside a string of text
+
+mu_num = -5
+print(abs(mu_num))
+# Prints out the absolute value
+
+print(pow(4, 6))
+# Prints out 4 to the power of 6, which is 4x4x4x4x4x4
+
+print(max(4, 6))
+# Prints out the highest number. Opposite of this is "min"
+
+print(math.floor(3.7))
+# Prints out the lowest number. Opposite of this is "ceil"
+
+print(math.sqrt(36))
+# Prints square root of 36
