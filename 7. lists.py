a3b5ef6b0a72464675e04d18ab96c87ec612aa30
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-03-29 22:55:54 +0100

    Create 7. lists.py

---
+++ 7. lists.py
@@ -0,0 +1,17 @@
+names = ["Bill", "George", "John", "Paul", "Eric"]
+# Creates a list called "names"
+
+print(names)
+# Outputs full list to the scren
+
+print(names[0])
+# Outputs the name in index position 0 to the screen (in this case Bill)
+
+print(names[-1])
+# Outputs the name in index position -1 to the screen (in this case Eric). The -1 means we start from the rnd of the list. So if we used -2 it would output Paul
+
+print(names[1:])
+# Outputs all names after (and including) index position 1 to the screen
+
+print(names[1:4])
+# Outputs the name after index position 1 and up to (but not including) index position 4 to the screen
