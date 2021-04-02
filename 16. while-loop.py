c655f0ec6359fb3f58ec068a3d763917a7355ea3
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-04-02 22:53:12 +0100

    Create 16. while-loop.py

---
+++ 16. while-loop.py
@@ -0,0 +1,14 @@
+# Alows you to execute a block of code multiple times until a certain condition is made
+
+i = 1
+# Defines "i" as the number 1
+
+while i <= 10:
+# Anything inside this block is considered the while loop. This is saying while "i" is less than or equal to 10, keep checking the condition and executing the loop
+    print(i)
+    # This is going to output the value of "i" on the screen
+    i += 1
+    # This is going to add 1 to the value of "i" every time the loop executes
+
+print("Done with loop")
+# Eventually, "i" is going to end up being 11. so this will end the loop and output the "Done with loop" statement
