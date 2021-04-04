55d391ee537a69a7904b094d2594f446597c3fc5
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-04-04 12:47:42 +0100

    Create 23-modules_and_pip.py

---
+++ 23-modules_and_pip.py
@@ -0,0 +1,9 @@
+# Modules are Python files that can be imported into our current Python.
+# We can store functions, variables etc and import these when required
+# We can write our own modules, or else install external modules using "pip"
+# Any external modules that are installed with pip are stored in the "lib" folder under the Python install (eg C:\Program Files\Python39\Lib)
+# This location also contains lots of built-in modules for everyday tasks
+# There are hundreds of modules available at https://docs.python.org/3/py-modindex.html
+
+# pip comes preinstalled with Python3. To check for version, run "pip --version" from a command prompt
+# To install, run "pip install <module name>"
