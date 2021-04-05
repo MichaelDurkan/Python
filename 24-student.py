943045fb110b09e040347da6ee37ac2c2d504cb3
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-04-05 11:23:38 +0100

    Create 24-student.py

---
+++ 24-student.py
@@ -0,0 +1,8 @@
+class Student:
+# Creates a class called student
+    def __init__(self, name, course, grade, is_on_probation):
+    # This function defines what the "Student" "class/data type" is inside our program
+        self.name = name
+        self.course = course
+        self.grade = grade
+        self.is_on_probation = is_on_probation
