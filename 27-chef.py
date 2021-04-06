a5818f14c7ebca9c122169975f95c1b85a06d270
Michael Durkan <80047500+MichaelDurkan@users.noreply.github.com>
2021-04-06 10:42:42 +0100

    Create 27-chef.py

---
+++ 27-chef.py
@@ -0,0 +1,11 @@
+class Chef:
+# This is creating our "chef" class with the functions below
+
+    def make_chicken(self):
+        print ("The chef makes a chicken")
+
+    def make_salad(self):
+        print("The chef makes a salad")
+
+    def make_special(self):
+        print("The check makes the special")
